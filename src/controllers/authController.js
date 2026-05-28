53947ecc8738531fd879d02469eafc65f15593c6
Rolex091 <153530268+Rolex091@users.noreply.github.com>
2026-05-28 10:54:55 +0530

    Switch SMTP to port 587

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -11,8 +11,8 @@ console.log("SMTP DEBUG:");
 
 console.log({
   host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   user: process.env.SMTP_USER,
   passExists: !!process.env.SMTP_PASS,
 });
@@ -21,8 +21,8 @@ console.log({
 // ==========================
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
